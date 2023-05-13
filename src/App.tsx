ca959a47460a4a8591a71e30aa7891110fa41632
Brandon-Skarka <107329282+Brandon-Skarka@users.noreply.github.com>
2023-05-13 16:41:44 -0700

    Added routing (for multiple pages) and created a test route for / to the Home component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import React from 'react';
 import './App.css';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
