a09369e4a25704a80502ebdbdd06ef3a3ce2d364
Brandon-Skarka <107329282+Brandon-Skarka@users.noreply.github.com>
2023-05-13 17:06:45 -0700

    Updated to use browser router instead of hash router

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import React from 'react';
 import './App.css';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter basename="Workout-Tracker/">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />}/>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
