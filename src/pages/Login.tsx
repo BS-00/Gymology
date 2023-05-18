665f6bf6720f51e2551b3bcb8f58287600a6e799
Jan Kasen <jskasen@gmail.com>
2023-05-17 19:34:17 -0700

    Changed the login window position to the center

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,13 +3,15 @@ import { Button } from 'react-bootstrap';
 import TextBox from '../modules/TextBox';
 
 const Login = () => (
-  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '40vh' }}>
+  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '70vh' }}>
     <div>
       <h1>Login</h1>
       <div style={{ display: 'flex', flexDirection: 'column' }}>
         <TextBox label="Email" />
+        <div style={{ marginTop: '10px' }} />
         <TextBox label="Password" />
       </div>
+      <div style={{ marginTop: '10px' }} />
       <Button variant="primary">Login</Button>
     </div>
   </div>
