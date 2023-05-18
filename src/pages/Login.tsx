4c34f12e2522416e20a856dbf4e1a6eff75530a4
Jan Kasen <jskasen@gmail.com>
2023-05-17 20:52:51 -0700

    Fixed issue with the Login button

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
 import TextBox from '../modules/TextBox';
 
 const Login = () => (
   <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '70vh' }}>
     <div>
       <h1>Login</h1>
-      <div style={{ display: 'flex', flexDirection: 'column' }}>
-        <TextBox label="Email" />
-        <div style={{ marginTop: '10px' }} />
-        <TextBox label="Password" />
-      </div>
       <div style={{ marginTop: '10px' }} />
-      <Button variant="primary">Login</Button>
+      <TextBox label="Email" />
+      <div style={{ marginTop: '10px' }} />
+      <TextBox label="Password" />
+      <button type="submit" className="my-2 btn btn-primary">Login</button>
     </div>
   </div>
 );
