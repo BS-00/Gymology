3d4ee6c5d417de7826bacfff52eaa29c242f6546
Jan Kasen <jskasen@gmail.com>
2023-05-17 17:01:06 -0700

    Added a text box module for email and password entry. Added a basic template for the login page. The textboxes can have the text in them. The Login button does not do anything. Installed react-bootstap.

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import TextBox from '../modules/TextBox';
 
-function Login(): React.ReactElement {
-  return (
-    <p>Login Page</p>
-  );
-}
+const Login = () => (
+  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '40vh' }}>
+    <div>
+      <h1>Login</h1>
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+        <TextBox label="Email" />
+        <TextBox label="Password" />
+      </div>
+      <Button variant="primary">Login</Button>
+    </div>
+  </div>
+);
 
 export default Login;
