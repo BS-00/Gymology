ee28ffb59f121c57a75cd963c5749a99f9319bff
Jesus Muniz <munizzjesus04@gmail.com>
2023-06-01 18:01:24 -0700

    Created a themes.css file to modify the theme of a page. Implemented it into App.tsx to have it modify the whole website.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import React from 'react';
+import React, { useState } from 'react';
 import AppRouter from './modules/AppRouter';
 import NavBar from './modules/NavBar';
 import Footer from './modules/Footer';
+import './themes.css';
 
 function App() {
   return (
