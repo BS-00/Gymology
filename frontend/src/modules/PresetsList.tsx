4e6994a10fe51f677e708ada59b0ebfc75385200
Dawit Eskinder Taddese <dboss7233@Dawits-MacBook-Air.local>
2023-06-01 18:56:30 -0700

    Presets page shows selected preset workout content
    A new, react Preset interface was created to hold its necessary string and number data with useEffect imported from React to aid the side effect processing in PresetsList.tsx by asynchronously passing the default workout plan examples: Beginner, Intermediate, and Advanced to the presets state variable. Its optional dependency was the empty array ‘[]’ for now since it doesn’t need to re-render for any changes because the backend/api isn’t fully connected to more workout plans being queried from the database. Side-by-side, the presets-list and selectedpreset-contents are returned. The logic for the presets-list column is similar with the checked and the onChange reflecting the state of the selected preset as it maps and iterates through plan_name variables. If there’s a selected preset, the days_of_the_week strings are printed out continually with a comma and space delimiting, and then it maps through the index of the array holding workouts to print out the name, sets, reps, and weights of each in unordered list format.

--- frontend/src/modules/PresetsList.tsx
+++ frontend/src/modules/PresetsList.tsx
@@ -1,3 +1,160 @@
+
+import React, { useEffect, useState } from 'react';
+
+interface Preset {
+  plan_name: string;
+  days_of_the_week: string[];
+  workouts: Workout[];
+}
+
+interface Workout {
+  workout_name: string;
+  sets: number;
+  reps: number;
+  weight: number;
+}
+
+const PresetsList: React.FC = () => {
+  const [presets, setPresets] = useState<Preset[]>([]);
+  const [selectedPreset, setSelectedPreset] = useState<Preset | null>(null);
+
+  useEffect(() => {
+    const availablePresets: Preset[] = [
+      {
+        plan_name: 'Beginner',
+        days_of_the_week: ['Monday', 'Wednesday', 'Friday'],
+        workouts: [
+          {
+            workout_name: 'Squats',
+            sets: 3,
+            reps: 10,
+            weight: 50,
+          },
+          {
+            workout_name: 'Push-ups',
+            sets: 3,
+            reps: 10,
+            weight: 0,
+          },
+        ],
+      },
+      {
+        plan_name: 'Intermediate',
+        days_of_the_week: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+        workouts: [
+          {
+            workout_name: 'Bench Press',
+            sets: 4,
+            reps: 8,
+            weight: 80,
+          },
+          {
+            workout_name: 'Deadlifts',
+            sets: 4,
+            reps: 8,
+            weight: 100,
+          },
+          {
+            workout_name: 'Pull-ups',
+            sets: 4,
+            reps: 6,
+            weight: 0,
+          },
+        ],
+      },
+      {
+        plan_name: 'Advanced',
+        days_of_the_week: ['Monday', 'Wednesday', 'Friday', 'Saturday'],
+        workouts: [
+          {
+            workout_name: 'Squat Clean',
+            sets: 5,
+            reps: 5,
+            weight: 120,
+          },
+          {
+            workout_name: 'Snatch',
+            sets: 5,
+            reps: 5,
+            weight: 100,
+          },
+          {
+            workout_name: 'Handstand Push-ups',
+            sets: 5,
+            reps: 8,
+            weight: 0,
+          },
+          {
+            workout_name: 'Plank',
+            sets: 3,
+            reps: 30,
+            weight: 0,
+          },
+        ],
+      },
+    ];
+
+    setPresets(availablePresets);
+  }, []);
+
+  const handlePresetChange = (preset: Preset) => {
+    setSelectedPreset(preset);
+  };
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-6">
+          <div className="presets-list">
+            <h2>Presets</h2>
+            <ul>
+              {presets.map((preset) => (
+                <li key={preset.plan_name}>
+                  <input
+                    type="checkbox"
+                    checked={selectedPreset && (selectedPreset.plan_name === preset.plan_name) ? true : false}
+                    onChange={() => handlePresetChange(preset)}
+                  />
+                  <label>{preset.plan_name}</label>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <div className="col-6">
+          <div className="selectedpreset-contents">
+            <h2>Selected Preset</h2>
+            {selectedPreset ? (
+              <div>
+                <h3>{selectedPreset.plan_name}</h3>
+                <p>
+                  Days of the week: {selectedPreset.days_of_the_week.join(', ')}
+                </p>
+                <h4>Workouts:</h4>
+                <ul>
+                  {selectedPreset.workouts.map((workout, index) => (
+                    <li key={index}>
+                      <p>Workout Name: {workout.workout_name}</p>
+                      <p>Sets: {workout.sets}</p>
+                      <p>Reps: {workout.reps}</p>
+                      <p>Weight: {workout.weight}</p>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ) : (
+              <p>No preset selected</p>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PresetsList;
+
+/*
 import React, { useState } from 'react';
 
 function PresetsList(): React.ReactElement {
@@ -38,3 +195,4 @@ function PresetsList(): React.ReactElement {
 }
 
 export default PresetsList;
+*/
