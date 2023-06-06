54a7711fe88c65df61186896f15fdc62db3d7f43
Brandon-Skarka <107329282+Brandon-Skarka@users.noreply.github.com>
2023-06-06 05:44:03 -0700

    Added CORS Header for Cross Origin Support

--- backend/express/src/index.js
+++ backend/express/src/index.js
@@ -2,12 +2,23 @@ require('dotenv').config({path: '../.env'});
 //Initialize MySQL Database
 require('./databases/db').initDb();
 
+
 const express = require('express');
 const app = express();
 
+//Add cors headers (to prevent Access-Control-Allow-Origin header error)
+const cors = require('cors');
+app.use(cors({
+	origin: ['http://localhost:3000']
+}));
+
+//Add body parser so post requests can be read
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 //Routes
 app.use(require('./routes/create-workout'));
-
 
 //Express Server
 app.listen(process.env.EXPRESS_PORT, (err) => {
