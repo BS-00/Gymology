ee015673912795b683469050dda6db0d5910beca
Jesus Muniz <munizzjesus04@gmail.com>
2023-06-06 10:25:42 -0700

    Moved the location of the create button to the bottom right

--- frontend/src/pages/CreateWorkout.tsx
+++ frontend/src/pages/CreateWorkout.tsx
@@ -26,6 +26,29 @@ function ExerciseDisplay(props: {name: String}): React.ReactElement {
     );
 }
 
+function WorkoutForm(): React.ReactElement {
+  return (
+      <form className="container-fluid w-100">
+        <DaysOfWeekSelector id="1"/>
+        <div className="my-2 form-group">
+          <label htmlFor="workout-name-input" className="form-label">Name</label>
+          <input id="workout-name-input" type="text" className="form-control" placeholder="Name"/>
+        </div>
+
+        <div className="exercise-input">
+	  <div className="my-2 form-group">
+            <label htmlFor="exercise-name-input" className="form-label">Exercise Name</label>
+            <input id="exercise-name-input" type="text" className="form-control" placeholder="Exercise Name"/>
+	  </div>
+	  <IncNumInput id="sets-input" label="Sets" minVal={1} />
+          <IncNumInput id="reps-input" label="Reps" minVal={1} />
+          <IncNumInput id="weight-input" label="Weight (lbs)" minVal={0} amnt={10}/>
+        </div>
+        <button type="submit" className="my-5 btn btn-primary">Create</button>
+      </form>
+  );
+}
+
 function CreateWorkout(): React.ReactElement {
 	const [exerciseDisplays, setExerciseDisplays] = useState<Array<React.ReactElement>>([]);
 
@@ -90,10 +113,10 @@ function CreateWorkout(): React.ReactElement {
           </form>
         </div>
         <div className="m-3 w-50 h-75">
-          <h3 className="text text-center">Exercises</h3>
-          <button className="btn btn-primary" onClick={e => addExercise(e)}>Add Exercise</button>  
-          <div className="h-75 overflow-auto">
-            {exerciseDisplays}
+	  <h3 className="text text-center">Exercises</h3>
+	  <button className="btn btn-primary" onClick={e => addExercise(e)}>Add Exercise</button>  
+	  <div className="h-75 overflow-auto">
+	    {exerciseDisplays}
           </div>
         </div>
       </div>
