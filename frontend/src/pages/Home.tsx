dec66dfd3e76ff06fcedb1c8bc026f7293a26d58
Jesus Muniz <munizzjesus04@gmail.com>
2023-06-05 17:12:15 -0700

    Fixed Quality of Icon, and Theme Button location
    Replaced the Icon.png with a new png of better quality. Fixed the sizing of the Icon on the NavBar. Moved the theme changing button to the bottom right of the screen.

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,16 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect } from 'react';
 import ButtonComponent from '../modules/Button';
-//import imageSrc from '../modules/Logo.png'
-//import './themes.css'
-import imageSrc from '../modules/Icon.png'
+import imageSrc from '../modules/Icon.png';
+//import themeChanger from 'themes.tsx'
+import '../themes.css';
+
 
 
 
 function ButtonDisplay() {
   return (
     <>
-       <div style={{ position: 'fixed', top: '13%', marginLeft: '30%' }}>
-        <img src={imageSrc} alt="Image Description" style={{ width: '140%', height: 'auto', borderRadius: '10px' }} />
+       <div style={{ position: 'fixed', top: '17%', marginLeft: '30%' }}>
+        <img src={imageSrc} alt="Image Description" style={{ width: '40%', height: '100%', borderRadius: '10px', marginLeft: '28%' }} />
           <h1 style={{fontFamily: 'Eurostile Next Pro Bold',fontSize: '60px', position:'fixed',top: '75%', marginLeft: '24%'}}>
             GYMOLOGY
           </h1>
@@ -26,12 +27,31 @@ function ButtonDisplay() {
 }
 
 
+
+
 function Home(): React.ReactElement {
+
+  const [theme, setTheme] = useState('light');
+  const toggleTheme = () => {
+    if (theme === 'light') {
+      setTheme('dark');
+    } else {
+      setTheme('light');
+    }
+  };
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+
   return (
-    
     <>
+  
+
     <h1 className="display-5 text-center">Welcome to the Home Page!</h1>
     <ButtonDisplay/>
+    <div className={`App ${theme}`}>
+      <button className="btn btn-primary position-absolute bottom-0 end-0" style={{backgroundColor: 'black', borderColor: 'white'}} onClick={toggleTheme}>Toggle Theme</button>
+    </div>
     </>
   );
 }
