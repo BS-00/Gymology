b6ecab5e7ca33106c073e36d5049ec748986f589
Jesus Muniz <munizzjesus04@gmail.com>
2023-06-07 00:09:17 -0700

    Fixed Theme issue on Presets Page

--- frontend/src/pages/Presets.tsx
+++ frontend/src/pages/Presets.tsx
@@ -1,10 +1,18 @@
-import {useState} from "react";
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import PresetsList from '../modules/PresetsList';
 
 // Reminder to make Bootstrap enhancements here
 
 
 function Presets(): React.ReactElement {
+
+  const storedTheme = localStorage.getItem('theme');
+  	const [theme, setTheme] = useState(storedTheme || 'light');
+
+  	useEffect(() => {
+    	document.body.className = theme;
+  	}, [theme]);
+
   return (
     <div className="presets-page">
       <PresetsList />
