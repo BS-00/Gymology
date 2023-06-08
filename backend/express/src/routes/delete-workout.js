54e9e7b54be071b492d55546b8986d2ae96b7d20
BS-00 <107329282+BS-00@users.noreply.github.com>
2023-06-08 16:33:55 -0700

    Delete-Workout Now API Deletes Exercises

--- backend/express/src/routes/delete-workout.js
+++ backend/express/src/routes/delete-workout.js
@@ -5,12 +5,13 @@ const queryDb = require('../databases/db.js').queryDb;
 
 router.post('/delete-workout', async (req, res) => {
     const { uid, wid } = req.body;
-	const query_res = await queryDb(`DELETE FROM Workouts WHERE uid=${uid} AND wid=${wid};`);
+    const query1_res = await queryDb(`DELETE FROM Exercises WHERE wid='${wid}';`);
+    const query_res = await queryDb(`DELETE FROM Workouts WHERE uid='${uid}' AND wid='${wid}';`);
 
-	if(query_res === null ||
-	   query_res === undefined) {
-		throw new RangeError("Error removing workout");
-	}
+    if(query_res === null ||
+       query_res === undefined) {
+	throw new RangeError("Error removing workout");
+    }
 
     res.sendStatus(201);
 });
