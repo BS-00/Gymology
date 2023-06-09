834618d51c8c1a1a63a9e8f996f5ea000e67a5d3
Jan Kasen <jskasen@gmail.com>
2023-06-08 20:36:16 -0700

    Fixed issue with page overflow on the History page. Made the History box nicer.

--- frontend/src/pages/History.tsx
+++ frontend/src/pages/History.tsx
@@ -29,22 +29,34 @@ function History(): React.ReactElement {
 	}, []);
 
   return (
-    <div className="container mt-5">
-      <h1 className="display-4 text-center mb-4">Workout History</h1>
-      <div className="card border-0 shadow">
-        <div className="overflow-auto" style={{ maxHeight: '400px' }} id="historyContainer">
-          <ul className="list-group list-group-flush">
-            {histories.length > 0 ? (
-              histories.map((history, i) => (
-                <li className="list-group-item bg-transparent" key={i}>
-                  <h2 className="h5">{history.workout_name}</h2>
-                  <p className="mb-0">Date and Time: {history.completion_date}</p>
-                </li>
-              ))
-            ) : (
-              <li className="list-group-item">Loading workout data...</li>
-            )}
-          </ul>
+    <div className="container-fluid">
+      <div className="row justify-content-center">
+        <div className="col-8 mt-5">
+          <h1 className="display-4 text-center mb-4">Workouts Completed</h1>
+          <div className="card border-0 shadow">
+            <div
+              className="overflow-auto"
+              style={{ maxHeight: '400px', background: 'transparent' }}
+              id="historyContainer"
+            >
+              <ul className="list-group list-group-flush">
+                {histories.length > 0 ? (
+                  histories.map((history, i) => (
+                    <li
+                      className="list-group-item bg-transparent"
+                      style={{ background: 'transparent' }}
+                      key={i}
+                    >
+                      <h2 className="h5">{history.workout_name}</h2>
+                      <p className="mb-0">Date and Time: {history.completion_date}</p>
+                    </li>
+                  ))
+                ) : (
+                  <li className="list-group-item">Loading workout data...</li>
+                )}
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </div>
