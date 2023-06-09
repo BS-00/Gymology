6bbe3d71b18a63803bf3032ec840a2973622dda3
Jan Kasen <jskasen@gmail.com>
2023-06-08 21:27:03 -0700

    In history.js route made it so that the completion date converts to the local time.

--- backend/express/src/routes/history.js
+++ backend/express/src/routes/history.js
@@ -2,22 +2,23 @@ const express = require('express');
 const router = express.Router();
 const queryDb = require('../databases/db.js').queryDb;
 
-
 router.post('/history', async (req, res) => {
     const completion_histories = await queryDb(`SELECT * FROM History WHERE uid='${req.body.uid}';`);
-	
-    if(completion_histories === null ||
-       completion_histories === undefined) {
-		throw new RangeError("Error getting completed workouts");
+
+    if (completion_histories === null || completion_histories === undefined) {
+        throw new RangeError("Error getting completed workouts");
     }
 
     res.send({
-		histories: completion_histories.map(h => {
-			return {
-				workout_name: h.workout_name,
-				completion_date: h.completion_date
-			}
-		})
+        histories: completion_histories.map(h => {
+            const completionDate = new Date(h.completion_date);
+            const localCompletionDate = completionDate.toLocaleString(); // Convert to local time
+
+            return {
+                workout_name: h.workout_name,
+                completion_date: localCompletionDate
+            };
+        })
     });
 });
 
